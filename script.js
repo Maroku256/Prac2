c9900063d8d871b7aff00310cc7d17833974b39c
Maroku256 <81808337+Maroku256@users.noreply.github.com>
2023-01-13 15:25:27 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -146,3 +146,8 @@ console.log(sum3)
 // Напишите функцию сортировки my(a,b), которая при вызове d4.sort(my) 
 // отсортирует элементы массива по убыванию чисел. Вызовите d4.sort(my)
 
+let d4 = [45, 78, 10, 3]
+function my(a, b){
+    return (b - a)
+}
+console.log(d4.sort(my))
